f12184fa26d1eee44e9749f6f1f8614e4f401a55
Sergey Bochkov <bochkovsa@gmail.com>
2019-06-28 14:35:36 +0500

    cleanup logging

--- src/main/java/sb/tasks/pages/IndexPage.java
+++ src/main/java/sb/tasks/pages/IndexPage.java
@@ -1,15 +1,10 @@
 package sb.tasks.pages;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 import org.quartz.Scheduler;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import ratpack.handling.Context;
 import ratpack.jackson.Jackson;
-import ratpack.jackson.JsonRender;
 import sb.tasks.system.SchedulerInfo;
 
 import java.util.ArrayList;
@@ -17,25 +12,18 @@ import java.util.List;
 
 public final class IndexPage implements HttpPage {
 
-    private static final Logger LOG = LoggerFactory.getLogger(IndexPage.class);
-
     private final MongoDatabase db;
     private final Scheduler scheduler;
-    private final ObjectMapper jackson;
 
     public IndexPage(MongoDatabase db, Scheduler scheduler) {
         this.db = db;
         this.scheduler = scheduler;
-        this.jackson = new ObjectMapper();
     }
 
     @Override
     public void handle(Context ctx) {
-        LOG.info("index page: start handling");
         List<Document> docs = new ArrayList<>();
-        LOG.info("index page: find documents");
         db.getCollection("tasks").find().into(docs);
-        LOG.info("index page: docs found");
         SchedulerInfo schInfo = new SchedulerInfo(scheduler);
         for (Document doc : docs) {
             doc.put("oid", doc.getObjectId("_id").toString());
@@ -46,18 +34,6 @@ public final class IndexPage implements HttpPage {
                     )
             );
         }
-        LOG.info("index page: complete computed values, start rendering");
-        JsonRender render = Jackson.json(docs);
-        LOG.info("index page: json render complete");
-
-        try {
-            jackson.writeValueAsString(docs);
-        } catch (JsonProcessingException ex) {
-            LOG.warn(ex.getMessage());
-        }
-        LOG.info("index page: json render objectmapper complete");
-
-        ctx.render(render);
-        LOG.info("index page: end handling");
+        ctx.render(Jackson.json(docs));
     }
 }
