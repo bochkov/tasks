343c9e1a332f12687be85943bd8cb7314e058f75
Sergey Bochkov <bochkovsa@gmail.com>
2019-08-14 14:13:41 +0500

    removed test code

--- src/main/java/sb/tasks/jobs/dailypress/agent/AnSportExpress.java
+++ src/main/java/sb/tasks/jobs/dailypress/agent/AnSportExpress.java
@@ -76,29 +76,4 @@ public final class AnSportExpress implements Agent<MagResult> {
                 new MagResult(out, url, document.get("params", Document.class).getString("text"))
         );
     }
-
-    public static void main(String[] args) throws Exception {
-        String url = Jsoup.connect("http://www.sport-express.ru/newspaper/").get()
-                .getElementsByClass("se19-newspaper").first()
-                .getElementsByAttribute("data-newspaper-link").first()
-                .attr("href");
-        System.out.println(String.format("Checking link: %s", url));
-        File out = new File(
-                String.format("se%s.pdf", new SimpleDateFormat("yyyyMMdd").format(new Date()))
-        );
-        Response response = new JdkRequest(url)
-                .through(RetryWire.class)
-                .through(CookieOptimizingWire.class)
-                .through(AutoRedirectingWire.class)
-                .header("Upgrade-Insecure-Requests", "1")
-                .header(HttpHeaders.ACCEPT, "application/pdf")
-                .header(HttpHeaders.USER_AGENT, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36")
-                .header(HttpHeaders.COOKIE, "seuid=%7B%22id%22%3A1121911%7D")
-//                .header(HttpHeaders.COOKIE, String.format("PHPSESSID=%s", phpSessId))
-//                .header(HttpHeaders.COOKIE, String.format("se.sess=%s", sess))
-//                .header(HttpHeaders.COOKIE, String.format("se.user=%s", user))
-                .fetch();
-        System.out.println(response.headers());
-        new PdfFromResponse(response).saveTo(out);
-    }
 }
