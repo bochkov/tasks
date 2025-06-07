4ad255c5267e1be4de20a0dd5b7cee5341d226b2
Sergey Bochkov <bochkovsa@gmail.com>
2025-06-07 19:31:40 +0500

    add chromedriver with apk

--- src/main/java/sb/tasks/configuration/Selenium.java
+++ src/main/java/sb/tasks/configuration/Selenium.java
@@ -1,39 +1,24 @@
 package sb.tasks.configuration;
 
-import kong.unirest.core.JsonNode;
-import kong.unirest.core.Unirest;
-import kong.unirest.core.json.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.boot.context.event.ApplicationStartingEvent;
-import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 import sb.tasks.model.Property;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.util.List;
 import java.util.Map;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
 
 @Slf4j
 @Component
-public class Selenium implements ApplicationListener<ApplicationReadyEvent> {
+public class Selenium {
 
-    private static final String DRIVER_FILENAME = "chromedriver";
-    private static final File DRIVER_FILE = new File(Property.TMP_DIR, DRIVER_FILENAME);
+    private static final String DRIVER_FILENAME = "/usr/bin/chromedriver";
 
     public WebDriver createWebDriver() {
         LOG.debug("init chromium driver");
-        String chromeDriverPath = DRIVER_FILE.getPath();
-        System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+        System.setProperty("webdriver.chrome.driver", DRIVER_FILENAME);
         ChromeOptions options = new ChromeOptions();
         options.addArguments(List.of(
                 "--headless",
@@ -48,57 +33,4 @@ public class Selenium implements ApplicationListener<ApplicationReadyEvent> {
         ));
         return new ChromeDriver(options);
     }
-
-    private String getDriverUrl() {
-        JsonNode json = Unirest.get("https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions-with-downloads.json")
-                .asJson()
-                .getBody();
-        Iterable<Object> platforms = json.getObject()
-                .getJSONObject("channels")
-                .getJSONObject("Stable")
-                .getJSONObject("downloads")
-                .getJSONArray(DRIVER_FILENAME);
-        for (Object target : platforms) {
-            if (target instanceof JSONObject jo && "linux64".equals(jo.getString("platform"))) {
-                return jo.getString("url");
-            }
-        }
-        return null;
-    }
-
-    // download chromium driver
-    @Override
-    public void onApplicationEvent(ApplicationReadyEvent ev) {
-        if (Files.exists(DRIVER_FILE.toPath())) {
-            LOG.info("Chromium Driver found in '{}'", DRIVER_FILE.getPath());
-            return;
-        }
-        LOG.info("Start install chromium driver");
-        String driverUrl = getDriverUrl();
-        if (driverUrl == null) {
-            LOG.warn("!! CANT INSTALL CHROMIUM DRIVER, URL NOT FOUND");
-            return;
-        }
-
-        byte[] bytes = Unirest.get(driverUrl).asBytes().getBody();
-        LOG.info("Downloaded driver, total bytes = {}", bytes.length);
-        try (ZipInputStream zis = new ZipInputStream(new ByteArrayInputStream(bytes))) {
-            ZipEntry entry;
-            byte[] buffer = new byte[1024];
-            while ((entry = zis.getNextEntry()) != null) {
-                LOG.info(entry.getName());
-                if (entry.getName().equals("chromedriver-linux64/" + DRIVER_FILENAME)) {
-                    try (FileOutputStream fOut = new FileOutputStream(DRIVER_FILE)) {
-                        int len;
-                        while ((len = zis.read(buffer)) > 0) {
-                            fOut.write(buffer, 0, len);
-                        }
-                    }
-                }
-            }
-        } catch (IOException ex) {
-            LOG.warn(ex.getMessage(), ex);
-        }
-    }
-
 }
